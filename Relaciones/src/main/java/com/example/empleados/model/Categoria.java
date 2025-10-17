8efa0a899551d45b687657bb88dcfd76c2f30404
Renesls <166040263+Renesls@users.noreply.github.com>
2025-10-16 20:30:23 -0600

    primer commit

--- Relaciones/src/main/java/com/example/empleados/model/Categoria.java
+++ Relaciones/src/main/java/com/example/empleados/model/Categoria.java
@@ -30,27 +30,27 @@ public class Categoria {
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public String getNombre() {
         return nombre;
     }
-    
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
     public List<Libro> getLibros() {
         return libros;
     }
-    
+
     public void setLibros(List<Libro> libros) {
         this.libros = libros;
     }
-    
+
     @Override
     public String toString() {
         return "Categoria{" +
